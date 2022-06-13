41e18dc433492b0c2ebf8f0342648446f6dd0be2
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2022-06-13 18:53:48 +0300

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        },
+        'css-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    static: './dist',
+    port: 5000,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
